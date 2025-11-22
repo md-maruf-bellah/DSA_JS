061bc73b2e4128c0764234404ca64d9422ed1d8f
md-maruf-bellah <marufbellah5511507@gmail.com>
2025-11-22 22:17:20 +0600

    2 day started

---
+++ 1_day/index.js
@@ -0,0 +1,19 @@
+// Day 1 — Variables, Data Types, Input/Output
+
+// দুইটা নাম্বার যোগ করার ফাংশন
+// কোনো সংখ্যাকে ৩ দিয়ে গুণ
+
+
+function TowNumber(a, b) {
+    let result = a + b;
+    return result;
+};
+// console.log(TowNumber(3, 3));
+
+
+function ThreeMultiple(a, b) {
+    let result = a * b;
+    return result;
+}
+
+console.log(ThreeMultiple(3, 4));
